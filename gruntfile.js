a03d491900b30c3fc36d7198237dc0cd40fcaa5c
Tino Goratsch <mail@goratschwebdesign.de>
2025-03-03 18:37:55 +0100

    chore: update sass API call to re-enable CSS file minification

--- gruntfile.js
+++ gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
         sass: {
             options: {
                 implementation: sass,
-                outputStyle: 'compressed',
+                style: 'compressed',
                 includePaths: [
                     'src/css',
                     'libs'
